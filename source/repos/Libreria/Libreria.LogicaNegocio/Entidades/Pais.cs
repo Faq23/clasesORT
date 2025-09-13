3b8305621c276c2c9c6486ad759bcc3d62481a16
Faq23 <martinez.eze23@gmail.com>
2025-09-13 11:32:21 -0300

    Primer Commit

---
+++ source/repos/Libreria/Libreria.LogicaNegocio/Entidades/Pais.cs
@@ -0,0 +1,17 @@
+﻿using Libreria.LogicaNegocio.Excepciones;
+using Libreria.LogicaNegocio.InterfacesDominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria.LogicaNegocio.Entidades
+{
+    public class Pais : IEntity
+    {
+        public int ID { get; set; }
+        public Nombre Nombre { get; set; }
+        public CantidadHabitantes CantidadHabitantes { get; set; }
+    }
+}
---
+++ source/repos/Libreria/Libreria.LogicaNegocio/Vo/Nombre.cs
@@ -0,0 +1,31 @@
+﻿using Libreria.LogicaNegocio.Excepciones;
+using Libreria.LogicaNegocio.InterfacesDominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria.LogicaNegocio.Vo
+{
+    public class Nombre : IValidar
+    {
+        public string Value { get; private set; }
+
+        public Nombre(string value)
+        {
+            Value = value;
+
+            Validar();
+        }
+
+        public void Validar()
+        {
+            if (string.IsNullOrEmpty(Value))
+            {
+                throw new NombreException();
+            }
+
+        }
+    }
+}
